ca0342140982b7dbdd2d6ca72a6484579bea44b6
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-02-05 16:34:44 +0000

    Add shopping list to recipe app

---
+++ app/controllers/shoppingListController.rb
@@ -0,0 +1,15 @@
+class ShoppingListsController < ApplicationController
+    def create
+      @shopping_list = ShoppingList.create(shopping_list_params)
+      @shopping_list.items.create(name: "Pinapple")
+      @shopping_list.items.create(name: "Apple")
+      @shopping_list.items.create(name: "Chicken breasts")
+      redirect_to @shopping_list
+    end
+  
+    private
+  
+    def shopping_list_params
+      params.require(:shopping_list).permit(:name)
+    end
+  end
---
+++ app/models/shopping_list.rb
@@ -0,0 +1,3 @@
+class ShoppingList < ApplicationRecord
+    has_many :items
+end
