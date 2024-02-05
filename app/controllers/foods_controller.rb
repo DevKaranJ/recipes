00cf6486697fe0323bf769b69a87ea10c066421b
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-02-05 14:46:29 +0000

    add method to create a food list for user

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,7 @@
 class FoodsController < ApplicationController
-end
+    def destroy
+      @food = Food.find(params[:id])
+      @food.update(deleted: true)
+      redirect_to foods_url
+    end
+  end
