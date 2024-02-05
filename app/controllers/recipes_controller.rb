951a6eaca4fdd43ed03dd7c016735bcca86186c3
Karan Joshi <Karan190213@gmail.com>
2024-02-05 22:15:34 +0530

    added function to create new recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,9 +15,34 @@ class RecipesController < ApplicationController
     @recipe.update(public: !@recipe.public)
     redirect_to @recipe, notice: "Recipe visibility updated successfully"
   end
+
+  def new
+    @recipe = current_user.recipes.build
+  end
+
+  def create
+    @recipe = current_user.recipes.build(recipe_params)
+
+    if @recipe.save
+      redirect_to user_recipe_path(@user, @recipe), notice: "Recipe was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @recipe = current_user.recipes.find(params[:id])
+    @recipe.destroy
+    redirect_to user_recipes_path(@user), notice: "Recipe was successfully deleted."
+  end
+  
   private
 
   def set_user
     @user = User.find(params[:user_id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :preparation_time, :cooking_time, :public)
   end
 end
