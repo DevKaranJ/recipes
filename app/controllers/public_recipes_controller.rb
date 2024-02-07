ec791ad4850aaa6a785488f21b9baa3edaa3d6ee
Karan Joshi <Karan190213@gmail.com>
2024-02-07 17:32:35 +0530

    added the public recipe to the public recipe list

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,7 +1,10 @@
+# app/controllers/public_recipes_controller.rb
 class PublicRecipesController < ApplicationController
   def index
+    @public_recipes = Recipe.where(public: true)
   end
 
   def show
+    @recipe = Recipe.find(params[:id])
   end
 end
