507b0bf19af94bba376a6656446fcf40a07646e4
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-02-07 15:21:37 +0000

    Update: fix linters

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,28 +1,26 @@
 class FoodsController < ApplicationController
   before_action :authenticate_user!
-  
-    def index
-      @foods = current_user.foods
-    end
-  
-    def new
-      @food = current_user.foods.build
-    end
-  
-    def create
-      @food = current_user.foods.build(food_params)
-      if @food.save
-        redirect_to user_foods_path(current_user), notice: 'Food was successfully created.'
-      else
-        render :new
-      end
-    end
-  
-    private
-  
-    def food_params
-      params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+
+  def index
+    @foods = current_user.foods
+  end
+
+  def new
+    @food = current_user.foods.build
+  end
+
+  def create
+    @food = current_user.foods.build(food_params)
+    if @food.save
+      redirect_to user_foods_path(current_user), notice: 'Food was successfully created.'
+    else
+      render :new
     end
   end
-  
-  
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
+end
