7f73451037345759d065c3c0a60e699106a01362
Karan Joshi <Karan190213@gmail.com>
2024-02-07 13:26:08 +0530

    edit and remove logics for ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,5 +1,6 @@
 class IngredientsController < ApplicationController
   before_action :set_recipe
+  before_action :set_ingredient, only: [:edit, :update, :destroy]
 
   def new
     @ingredient = @recipe.ingredients.build
@@ -14,11 +15,30 @@ class IngredientsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @ingredient.update(ingredient_params)
+      redirect_to user_recipe_path(@recipe.user, @recipe), notice: 'Ingredient was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @ingredient.destroy
+    redirect_to user_recipe_path(@recipe.user, @recipe), notice: 'Ingredient was successfully deleted.'
+  end
 
   private
 
   def set_recipe
     @recipe = Recipe.find(params[:recipe_id])
+  end
+
+  def set_ingredient
+    @ingredient = @recipe.ingredients.find(params[:id])
   end
 
   def ingredient_params
