0670993d32ed820401eede25cb3dd5514ad9435b
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-02-07 15:16:30 +0000

    add foodlist folder and files to recipe application

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,33 +1,28 @@
 class FoodsController < ApplicationController
-  def index
-    @user = User.includes(:foods).find(params[:user_id])
-    @foods = @user.foods
-  end
-
-  def new
-    @user = current_user
-    @food = @user.foods.build
-  end
-
-  def create
-    @food = current_user.foods.build(food_params)
-    if @food.save
-      redirect_to user_foods_path(current_user)
-    else
-      render :new
+  before_action :authenticate_user!
+  
+    def index
+      @foods = current_user.foods
+    end
+  
+    def new
+      @food = current_user.foods.build
+    end
+  
+    def create
+      @food = current_user.foods.build(food_params)
+      if @food.save
+        redirect_to user_foods_path(current_user), notice: 'Food was successfully created.'
+      else
+        render :new
+      end
+    end
+  
+    private
+  
+    def food_params
+      params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
     end
   end
-
-  def destroy
-    @food = current_user.foods.find(params[:id])
-    RecipeFood.where(food_id: @food.id).destroy_all
-    @food.destroy
-    redirect_to user_foods_path(current_user), notice: 'Food successfully deleted'
-  end
-
-  private
-
-  def food_params
-    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
-  end
-end
+  
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,22 @@
 Rails.application.routes.draw do
-  get 'foods/index'
-  get 'foods/show'
+  get 'public_recipes/index'
+  get 'public_recipes/show'
+get '/food_list', to: 'foods#index', as: 'food_list'
   get 'home/index'
   devise_for :users
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
   root to: "home#index"
 
-  patch 'update_name', to: 'users#update_name', as: 'update_name'
-  
   resources :users, only: [:index] do
     resources :recipes, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+resources :foods
+    patch 'update_name', to: 'users#update_name', as: 'update_name'
   end
 
   resources :recipes, only: [] do
+    resources :ingredients, only: [:new, :create, :edit, :update, :destroy]
     patch 'toggle_public', on: :member
   end
 
-  resources :public_recipes, only: [:index, :show, :destroy] do
-    member do
-      delete 'destroy_public_recipe', as: 'destroy_public'
+  resources :public_recipes, only: [:index, :show,]
     end
-  end
-end
