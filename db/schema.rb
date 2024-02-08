04faf459b110fbb8055bdad8d214ceba3d0e1431
Karan Joshi <Karan190213@gmail.com>
2024-02-08 13:04:46 +0530

    updated the ingredients page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_05_150806) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_08_072857) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_05_150806) do
     t.bigint "recipe_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "quantity_unit"
     t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
   end
 
@@ -55,12 +56,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_05_150806) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_recipes_on_user_id"
-  end
-
-  create_table "shopping_lists", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
