1dddaa6a25ab45433574ec9648402e6083bae5c7
Karan Joshi <Karan190213@gmail.com>
2024-02-09 19:17:18 +0530

    updated the inventory

--- app/controllers/inventory_foods_controller.rb
+++ app/controllers/inventory_foods_controller.rb
@@ -4,7 +4,14 @@ class InventoryFoodsController < ApplicationController
   before_action :set_inventory
 
   def create
-    @inventory_food = @inventory.inventory_foods.build(inventory_food_params)
+    food_name = inventory_food_params[:food_name]
+    quantity = inventory_food_params[:quantity]
+
+    # Find or create the Food record based on the provided food_name
+    food = Food.find_or_create_by(name: food_name)
+
+    # Create the InventoryFood record with the correct food_id
+    @inventory_food = @inventory.inventory_foods.build(food_id: food.id, quantity: quantity)
 
     if @inventory_food.save
       redirect_to user_inventory_path(@user, @inventory), notice: 'Food was successfully added to the inventory.'
